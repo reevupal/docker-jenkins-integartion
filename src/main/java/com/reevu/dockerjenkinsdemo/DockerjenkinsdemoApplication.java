78b7062bdc5becd1eab1c2d743173f71aa6092d9
reevupal07 <65147424+reevupal07@users.noreply.github.com>
2021-03-05 00:23:25 +0530

    first commit

---
+++ src/main/java/com/reevu/dockerjenkinsdemo/DockerjenkinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.reevu.dockerjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reevu/dockerjenkinsdemo/DockerjenkinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reevu.dockerjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
